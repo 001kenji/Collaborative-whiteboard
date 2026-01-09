05794ba8a6910b3706c2e9cd7ce6eefac732c343
001kenji <briannjuguna694@gmail.com>
2026-01-09 13:44:18 +0300

    configurations

--- server/main.py
+++ server/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Collaborative Whiteboard API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://your-frontend.com",
+        "https://whiteboard-meet.netlify.app",
         "http://localhost:3000"
     ],
     allow_credentials=True,
